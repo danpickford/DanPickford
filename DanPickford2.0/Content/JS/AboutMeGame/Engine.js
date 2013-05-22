e80c39fb846afdf008f78db814adcd5ac8bcdab3
dan-downunda <daniel.pickford@15below.com>
2013-05-22 16:28:39 +1000

    EOD Commit

--- DanPickford2.0/Content/JS/AboutMeGame/Engine.js
+++ DanPickford2.0/Content/JS/AboutMeGame/Engine.js
@@ -1,41 +1,44 @@
-﻿// To display a game over / game won popup box, 
-// create a endGame scene that takes in a `label` option
-// to control the displayed message.
-Q.scene('endGame', function (stage) {
-    //var container = stage.insert(new Q.UI.Container({
-    //    x: Q.width / 2, y: Q.height / 2, fill: "rgba(0,0,0,0.5)"
-    //}));
+﻿Q.scene('endGame', function (stage) {
+    var container = stage.insert(new Q.UI.Container({
+        x: Q.width / 2, y: Q.height / 2, fill: "rgba(0,0,0,0.5)"
+    }));
 
-    //var button = container.insert(new Q.UI.Button({
-    //    x: 0, y: 0, fill: "#CCCCCC",
-    //    label: "Play Again"
-    //}))
-    //var label = container.insert(new Q.UI.Text({
-    //    x: 10, y: -10 - button.p.h,
-    //    label: stage.options.label
-    //}));
-    //// When the button is clicked, clear all the stages
-    //// and restart the game.
-    //button.on("click", function () {
+    var buttonPlayAgain = container.insert(new Q.UI.Button({
+        x: 0, y: 0, fill: "#CCCCCC",
+        label: "Play Again"
+    }))
+    var buttonCV = container.insert(new Q.UI.Button({
+        x: 0, y: buttonPlayAgain.p.h + 20, fill: "#CCCCCC",
+        label: "Just show me the CV already!"
+    }))
+    var label = container.insert(new Q.UI.Text({
+        x: 10, y: -10 - buttonPlayAgain.p.h,
+        label: stage.options.label,
+        color: '#fff'
+    }));
     
+    buttonPlayAgain.on("click", function () {
     Q.clearStages();
     Q.stageScene("level1", 1);
-    //$('#myName_container').css('display', 'none');
-    
-    //    Q.stageScene('level1');
-    //});
+    });
+    buttonCV.on("click", function () {
+        Q.clearStages();
+        $('#myName_container').css('display', 'hidden');
+    });
 
-    //// Expand the container to visibily fit it's contents
-    //// (with a padding of 20 pixels)
-    //container.fit(20);
+    container.fit(50,50);
 });
 
-Q.load("../../Content/Images/About/sprites.png, ../../Content/JSON/sprites.json, ../../Content/JSON/level1.json, ../../Content/JSON/level2.json, ../../Content/Images/About/tiles.png, ../../Content/Images/About/tiles2.png", function () {
+Q.scene('WouldYouLikeToPlay', function (stage) {
+
+});
+
+Q.load("../../Content/Images/About/aboutSprite.png, ../../Content/JSON/sprites.json, ../../Content/JSON/level1.json, ../../Content/JSON/level2.json, ../../Content/Images/About/tiles.png, ../../Content/Images/About/tiles2.png", function () {
         // Sprites sheets can be created manually
     Q.sheet("tiles", "../../Content/Images/About/tiles.png", { tilew: 32, tileh: 32 });
     Q.sheet("tiles2", "../../Content/Images/About/tiles2.png", { tilew: 32, tileh: 32 });
         // Or from a .json asset that defines sprite locations
-        Q.compileSheets("../../Content/Images/About/sprites.png", "../../Content/JSON/sprites.json");
+    Q.compileSheets("../../Content/Images/About/aboutSprite.png", "../../Content/JSON/sprites.json");
 
         // Finally, call stageScene to run the game
         Q.stageScene("level1");
---
+++ DanPickford2.0/Content/JS/AboutMeGame/Level1.js
@@ -0,0 +1,49 @@
+﻿Q.scene("level1", function (stage) {
+    //30
+    // Add in a tile layer, and make it the collision layer
+    stage.collisionLayer(new Q.TileLayer({
+        dataAsset: '../../Content/JSON/level1.json',
+        sheet: 'tiles'
+    }));
+
+    var label = stage.insert(new Q.UI.Text({
+        x: 230, y: 180,
+        label: "13 March 1986, Dan was born.",
+        size: 18,
+        family: 'Courier New'
+    }));
+    var label = stage.insert(new Q.UI.Text({
+        x: 1000, y: -30,
+        label: "June 2002 finished Bishop Bell C of E School and Started at Sussex Downs College a GNVQ in ICT.",
+        size: 18,
+        family: 'Courier New'
+    }));
+    var label = stage.insert(new Q.UI.Text({
+        x: 2500, y: 370,
+        label: "June 2003 completed GNVQ and started BTEC ICT Pratitioners at Sussex Downs, this is where I discovered programming!",
+        size: 18,
+        family: 'Courier New'
+    }));
+    var label = stage.insert(new Q.UI.Text({
+        x: 3350, y: 154,
+        label: "Level 1 complete off to UNI!",
+        size: 18,
+        family: 'Courier New'
+    }));
+    stage.insert(new Q.Step({ x: 1850, y: 360 }));
+    // Create the player and add them to the stage
+    var player = stage.insert(new Q.Player());
+
+    // Give the stage a moveable viewport and tell it
+    // to follow the player.
+    stage.add("viewport").follow(player);
+
+    // Add in a couple of enemies
+    stage.insert(new Q.Baddy({ x: 700, y: 0 }));
+    stage.insert(new Q.Baddy({ x: 800, y: 0 }));
+    stage.insert(new Q.Baddy({ x: 600, y: 0 }));
+    stage.insert(new Q.Baddy({ x: 1900, y: 0 }));
+    stage.insert(new Q.Baddy({ x: 2100, y: 0 }));
+    // Finally add in the tower goal
+    stage.insert(new Q.Goal({ x: 3540, y: 144 }));
+});
