7c6374b4095805c344b7310fbac7c46de7f4ff6e
dan-downunda <daniel.pickford@15below.com>
2013-06-19 17:26:38 +1000

    EOD Commit

--- DanPickford2.0/Content/JS/Parallax/Parallax.js
+++ DanPickford2.0/Content/JS/Parallax/Parallax.js
@@ -6,12 +6,39 @@
     button = $('.button');
     mywindow = $(window);
     htmlbody = $('html,body');
+    dataslide = 1;
+    scrolling = false;
+    //Setup waypoints plugin
+    slide.waypoint(function (event) {
+        //cache the variable of the data-slide attribute associated with each slide
+        dataslide = $(this).attr('data-slide');
+        if (event === 'up') {
+            if (dataslide - 1 == 0) {
+                dataslide = 1;
+            } else {
+                dataslide = dataslide - 1;
+            }
+        }
+    });
+    
+    (function () {
+        var timer;
+        $(window).bind('scroll', function () {
+            clearTimeout(timer);
+            timer = setTimeout(refresh, 1000);
+        });
+        var refresh = function () {
+            goToByScroll(dataslide);
+            //console.log('Stopped Scrolling');
+        };
+    })();
+
     //Create a function that will be passed a slide number and then will scroll to that slide using jquerys animate. The Jquery
     //easing plugin is also used, so we passed in the easing method of 'easeInOutQuint' which is available throught the plugin.
     function goToByScroll(dataslide) {
         htmlbody.animate({
             scrollTop: $('.slide[data-slide="' + dataslide + '"]').offset().top
-        }, 2000, 'easeInOutQuint');
+        }, 500, 'easeInOutQuint');
     }
     //When the user clicks on the button, get the get the data-slide attribute value of the button and pass that variable to the goToByScroll function
     button.click(function (e) {
