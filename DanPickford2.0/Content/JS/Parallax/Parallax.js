b10ed5e0a572d2458f16032822b98fade45c1d27
dan-downunda <daniel.pickford@15below.com>
2013-06-19 11:58:40 +1000

    Basic Parallax action working with Colors

---
+++ DanPickford2.0/Content/JS/Parallax/Parallax.js
@@ -0,0 +1,22 @@
+﻿jQuery(document).ready(function ($) {
+    //initialise Stellar.js
+    $(window).stellar();
+    //Cache some variables
+    slide = $('.slide');
+    button = $('.button');
+    mywindow = $(window);
+    htmlbody = $('html,body');
+    //Create a function that will be passed a slide number and then will scroll to that slide using jquerys animate. The Jquery
+    //easing plugin is also used, so we passed in the easing method of 'easeInOutQuint' which is available throught the plugin.
+    function goToByScroll(dataslide) {
+        htmlbody.animate({
+            scrollTop: $('.slide[data-slide="' + dataslide + '"]').offset().top
+        }, 2000, 'easeInOutQuint');
+    }
+    //When the user clicks on the button, get the get the data-slide attribute value of the button and pass that variable to the goToByScroll function
+    button.click(function (e) {
+        e.preventDefault();
+        dataslide = $(this).attr('data-slide');
+        goToByScroll(dataslide);
+    });
+});
---
+++ DanPickford2.0/Controllers/ParallaxController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DanPickford2._0.Controllers
+{
+    public class ParallaxController : Controller
+    {
+        //
+        // GET: /Parallax/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
